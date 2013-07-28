d8ac15e44362e3b01012dfecd19bf9129935eb1b
Paul Dingemans <paul@mathdoku.net>
2013-07-28 05:25:00 +0000

    Show puzzle complexity in the shared puzzle activity.

--- src/net/cactii/mathdoku/ui/SharedPuzzleActivity.java
+++ src/net/cactii/mathdoku/ui/SharedPuzzleActivity.java
@@ -76,6 +76,27 @@ public class SharedPuzzleActivity extends AppFragmentActivity {
 
 		// Load the grid into the grid view.
 		((GridView) findViewById(R.id.gridView)).loadNewGrid(mGrid);
+		
+		// Display the difficulty rating.
+		final VerticalRatingBar puzzleParameterDifficultyRatingBar = (VerticalRatingBar) findViewById(R.id.puzzleParameterDifficultyRatingBar);
+		puzzleParameterDifficultyRatingBar.setEnabled(false);
+		switch (mGrid.getPuzzleComplexity()) {
+		case VERY_EASY:
+			puzzleParameterDifficultyRatingBar.setNumStars(1);
+			break;
+		case EASY:
+			puzzleParameterDifficultyRatingBar.setNumStars(2);
+			break;
+		case NORMAL:
+			puzzleParameterDifficultyRatingBar.setNumStars(3);
+			break;
+		case DIFFICULT:
+			puzzleParameterDifficultyRatingBar.setNumStars(4);
+			break;
+		case VERY_DIFFICULT:
+			puzzleParameterDifficultyRatingBar.setNumStars(5);
+			break;
+		}
 	}
 
 	@Override
