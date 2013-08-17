b1a7d8fde7f1c991238814a3fbfebea1a2c6e47f
Paul Dingemans <paul@mathdoku.net>
2013-08-17 08:01:14 +0000

    Hide the input mode image while playing the solved animation.

--- src/net/cactii/mathdoku/ui/PuzzleFragment.java
+++ src/net/cactii/mathdoku/ui/PuzzleFragment.java
@@ -833,6 +833,10 @@ public class PuzzleFragment extends android.support.v4.app.Fragment implements
 				setElapsedTime(mGrid.getElapsedTime());
 			}
 		}
+		if (mInputModeImageView != null) {
+			mInputModeImageView.setVisibility(View.INVISIBLE);
+			mInputModeImageView.invalidate();
+		}
 		if (mClearButton != null) {
 			mClearButton.setVisibility(View.GONE);
 			mClearButton.invalidate();
