46c32c4dd99099dd27e2b9b7cbb756ef43656eda
Paul Dingemans <paul@mathdoku.net>
2013-12-15 09:31:14 +0100

    Add the first (failing) unit test to the project.
    The build files have been restructured in order to be able to add unit
    tests to the project. Both building in Android Studio and from the command
    line is possible.
    Currently one failing unit test has been added as a proof that builds do
    not succeed in case of a failing unit test.

---
+++ app/src/test/java/net/mathdoku/plus/grid/CellChangeTest.java
@@ -0,0 +1,17 @@
+package net.mathdoku.plus.grid;
+
+		import org.junit.Test;
+		import org.junit.runner.RunWith;
+
+		import robolectric.RobolectricGradleTestRunner;
+
+		import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricGradleTestRunner.class)
+public class CellChangeTest {
+
+	@Test
+	public void shouldFail() {
+		assertTrue(false);
+	}
+}
