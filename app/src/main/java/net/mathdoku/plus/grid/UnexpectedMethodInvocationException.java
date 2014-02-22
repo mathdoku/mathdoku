476813fd201e9e097bb170a74929d36db23be210
Paul Dingemans <paul@mathdoku.net>
2014-02-22 15:26:54 +0100

    Refactor UnexpectedMethodInvocationException to resolve sonar warnings.

--- app/src/main/java/net/mathdoku/plus/grid/UnexpectedMethodInvocationException.java
+++ app/src/main/java/net/mathdoku/plus/grid/UnexpectedMethodInvocationException.java
@@ -1,21 +1,7 @@
 package net.mathdoku.plus.grid;
 
 public class UnexpectedMethodInvocationException extends RuntimeException {
-
-	private final String mErrorMessage;
-
-	public UnexpectedMethodInvocationException() {
-		super();
-		mErrorMessage = "unknown";
-	}
-
 	public UnexpectedMethodInvocationException(String errorMessage) {
 		super(errorMessage);
-		mErrorMessage = errorMessage; // save message
 	}
-
-	public String getError() {
-		return mErrorMessage;
-	}
-
 }
