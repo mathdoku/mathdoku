d19675a0df82e259a1d754fa296908413af9feff
Paul Dingemans <paul@mathdoku.net>
2014-04-25 21:36:16 +0200

    Prevent instantiation of utility class.

--- app/src/main/java/net/mathdoku/plus/griddefinition/GridDefinitionDelimiter.java
+++ app/src/main/java/net/mathdoku/plus/griddefinition/GridDefinitionDelimiter.java
@@ -3,4 +3,8 @@ package net.mathdoku.plus.griddefinition;
 class GridDefinitionDelimiter {
 	public static final String LEVEL1 = ":";
 	public static final String LEVEL2 = ",";
+
+	// Prevent instantiation of this utility class.
+	private GridDefinitionDelimiter() {
+	}
 }
