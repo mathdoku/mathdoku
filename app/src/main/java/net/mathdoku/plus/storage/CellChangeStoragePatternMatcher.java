16617e343f354ccaf0b573757a25178ca294a583
Paul Dingemans <paul@mathdoku.net>
2014-05-17 20:08:12 +0200

    Resolve sonar warnings.

--- app/src/main/java/net/mathdoku/plus/storage/CellChangeStoragePatternMatcher.java
+++ app/src/main/java/net/mathdoku/plus/storage/CellChangeStoragePatternMatcher.java
@@ -3,6 +3,7 @@ package net.mathdoku.plus.storage;
 import net.mathdoku.plus.storage.selector.StorageDelimiter;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,6 +23,7 @@ public class CellChangeStoragePatternMatcher {
 	private static final String REGEXP_OUTER = "^" + SAVE_GAME_CELL_CHANGE_LINE
 			+ StorageDelimiter.FIELD_DELIMITER_LEVEL1 + "(.*)$";
 	private static final int GROUP_CELL_CHANGE = 1;
+	private static final Pattern PATTERN_OUTER = Pattern.compile(REGEXP_OUTER);
 
 	// Regexp and groups inside. Groups 4 - 6 are helper groups which are
 	// needed to ensure the validity of the cell information but are not
@@ -31,16 +33,14 @@ public class CellChangeStoragePatternMatcher {
 			+ StorageDelimiter.FIELD_DELIMITER_LEVEL1 + "((\\d*)|((\\d*"
 			+ StorageDelimiter.FIELD_DELIMITER_LEVEL2 + ")+))"
 			+ StorageDelimiter.FIELD_DELIMITER_LEVEL1 + "(.*)\\]$";
-	final int GROUP_CELL_NUMBER = 1;
-	final int GROUP_PREVIOUS_USER_VALUE = 2;
-	final int GROUP_PREVIOUS_POSSIBLE_VALUES = 3;
-	final int GROUP_RELATED_CELL_CHANGED = 7;
-
-	private static final Pattern patternOuter = Pattern.compile(REGEXP_OUTER);
-	private static final Pattern patternInner = Pattern.compile(REGEXP_INNER);
+	private static final int GROUP_CELL_NUMBER = 1;
+	private static final int GROUP_PREVIOUS_USER_VALUE = 2;
+	private static final int GROUP_PREVIOUS_POSSIBLE_VALUES = 3;
+	private static final int GROUP_RELATED_CELL_CHANGED = 7;
+	private static final Pattern PATTERN_INNER = Pattern.compile(REGEXP_INNER);
 
 	public boolean matchesOuter(String line) {
-		matcherOuter = patternOuter.matcher(line);
+		matcherOuter = PATTERN_OUTER.matcher(line);
 		return matcherOuter.matches();
 	}
 
@@ -56,8 +56,8 @@ public class CellChangeStoragePatternMatcher {
 		return Integer.valueOf(matcherInner.group(GROUP_PREVIOUS_USER_VALUE));
 	}
 
-	public ArrayList<Integer> getPreviousPossibleValues() {
-		ArrayList<Integer> previousPossibleValues = new ArrayList<Integer>();
+	public List<Integer> getPreviousPossibleValues() {
+		List<Integer> previousPossibleValues = new ArrayList<Integer>();
 		if (!matcherInner.group(GROUP_PREVIOUS_POSSIBLE_VALUES).equals("")) {
 			for (String possible : matcherInner.group(
 					GROUP_PREVIOUS_POSSIBLE_VALUES).split(
@@ -68,59 +68,20 @@ public class CellChangeStoragePatternMatcher {
 		return previousPossibleValues;
 	}
 
-	public ArrayList<String> getRelatedCellChanges() {
-		return parseRelatedCellChanges(matcherInner
-				.group(GROUP_RELATED_CELL_CHANGED));
+	public List<String> getRelatedCellChanges() {
+		return new RelatedCellChangeSplitter(
+				matcherInner.group(GROUP_RELATED_CELL_CHANGED)).parse();
 	}
 
-	private ArrayList<String> parseRelatedCellChanges(
+	private List<String> parseRelatedCellChanges(
 			String concatenatedStringOfCellChanges) {
+		return new RelatedCellChangeSplitter(concatenatedStringOfCellChanges)
+				.parse();
 
-		// The related cell changes can not be matched using a regular
-		// expression because they recursively can contain other related
-		// cell changes.
-		ArrayList<String> relatedCellChanges = new ArrayList<String>();
-		int levelNestedGroup = 0;
-		int startPosGroup = 0;
-		int index = 0;
-		for (char c : concatenatedStringOfCellChanges.toCharArray()) {
-			switch (c) {
-			case '[': // Start of new group
-				if (levelNestedGroup == 0) {
-					// Remember starting position of outer group only
-					startPosGroup = index;
-				}
-				levelNestedGroup++;
-				break;
-			case ']':
-				levelNestedGroup--;
-				if (levelNestedGroup == 0) {
-					// Just completed a group.
-					String group = concatenatedStringOfCellChanges.substring(
-							startPosGroup, index + 1);
-					relatedCellChanges.add(group);
-				}
-				break;
-			default:
-				if (levelNestedGroup == 0
-						&& !Character.toString(c).equals(
-								StorageDelimiter.FIELD_DELIMITER_LEVEL2)) {
-					throw new IllegalStateException(
-							String
-									.format("Unexpected character '%c' at position %d in group '%s'.",
-											c, index,
-											concatenatedStringOfCellChanges));
-				}
-				break;
-			}
-			index++;
-		}
-
-		return relatedCellChanges;
 	}
 
 	public boolean matchesInner(String line) {
-		matcherInner = patternInner.matcher(line);
+		matcherInner = PATTERN_INNER.matcher(line);
 		return matcherInner.matches();
 	}
 
@@ -172,5 +133,72 @@ public class CellChangeStoragePatternMatcher {
 				.append("\n");
 
 		return stringBuilder.toString();
+	}
+
+	// The related cell changes can not be matched using a regular expression
+	// because they recursively can contain other related cell changes.
+	private class RelatedCellChangeSplitter {
+		private String concatenatedStringOfCellChanges;
+		private int levelNestedGroup;
+		private int startPosGroup;
+		private int index;
+		private List<String> relatedCellChanges;
+
+		public RelatedCellChangeSplitter(String concatenatedStringOfCellChanges) {
+			this.concatenatedStringOfCellChanges = concatenatedStringOfCellChanges;
+			relatedCellChanges = new ArrayList<String>();
+		}
+
+		public List<String> parse() {
+			levelNestedGroup = 0;
+			startPosGroup = 0;
+			index = 0;
+			for (char c : concatenatedStringOfCellChanges.toCharArray()) {
+				switch (c) {
+				case '[':
+					startGroup();
+					break;
+				case ']':
+					endGroup();
+					break;
+				default:
+					validateFieldDelimiter(c);
+					break;
+				}
+				index++;
+			}
+
+			return relatedCellChanges;
+		}
+
+		private void startGroup() {
+			if (levelNestedGroup == 0) {
+				// Remember starting position of outer group only
+				startPosGroup = index;
+			}
+			levelNestedGroup++;
+		}
+
+		private void endGroup() {
+			levelNestedGroup--;
+			if (levelNestedGroup == 0) {
+				// Just completed an outer group.
+				String group = concatenatedStringOfCellChanges.substring(
+						startPosGroup, index + 1);
+				relatedCellChanges.add(group);
+			}
+
+		}
+
+		private void validateFieldDelimiter(char c) {
+			if (levelNestedGroup == 0
+					&& !Character.toString(c).equals(
+							StorageDelimiter.FIELD_DELIMITER_LEVEL2)) {
+				throw new IllegalStateException(String.format(
+						"Unexpected character '%c' at position %d in group"
+								+ " '%s'.", c, index,
+						concatenatedStringOfCellChanges));
+			}
+		}
 	}
 }
