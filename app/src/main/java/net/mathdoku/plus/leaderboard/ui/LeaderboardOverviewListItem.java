e41a7cc4d0911eb386ea75c17eb9c4bdb1127adc
Paul Dingemans <paul@mathdoku.net>
2014-08-23 11:18:44 +0200

    Refactor class LeaderboardOverviewListItem

--- app/src/main/java/net/mathdoku/plus/leaderboard/ui/LeaderboardOverviewListItem.java
+++ app/src/main/java/net/mathdoku/plus/leaderboard/ui/LeaderboardOverviewListItem.java
@@ -26,12 +26,16 @@ import net.mathdoku.plus.util.Util;
  * displayed in a Leaderboard Overview list view.
  */
 public class LeaderboardOverviewListItem {
+	private final String mLeaderboardId;
 	private final int mGridSize;
-	private boolean mHideOperators;
-	private PuzzleComplexity mPuzzleComplexity;
-	private String mLeaderboardId;
-	private boolean mHasScore;
-	private final View mView;
+	private final boolean mHideOperators;
+	private final PuzzleComplexity mPuzzleComplexity;
+	private final View mLeaderboardListItemView;
+	private final LeaderboardOverviewListItemType leaderboardOverviewListItemType;
+
+	private enum LeaderboardOverviewListItemType {
+		WITH_SCORE, WITHOUT_SCORE, PLACEHOLDER_EMPTY_OVERVIEW
+	};
 
 	/**
 	 * Creates a new instance of a {@link LeaderboardOverviewListItem} for a
@@ -44,30 +48,21 @@ public class LeaderboardOverviewListItem {
 	 * @param puzzleComplexity
 	 *            The complexity level of the puzzle.
 	 */
-	public LeaderboardOverviewListItem(final LeaderboardOverview leaderboardOverview,
-									   int gridSize, boolean hideOperators,
-									   PuzzleComplexity puzzleComplexity) {
+	public LeaderboardOverviewListItem(
+			final LeaderboardOverview leaderboardOverview, int gridSize,
+			boolean hideOperators, PuzzleComplexity puzzleComplexity) {
 		mGridSize = gridSize;
 		mHideOperators = hideOperators;
 		mPuzzleComplexity = puzzleComplexity;
 
-		// Get the view and the layout to store the leaderboard section
-		mView = leaderboardOverview.getLayoutInflater().inflate(
-				R.layout.leaderboard_section, null);
+		// Inflate the view to display one leaderboard
+		mLeaderboardListItemView = leaderboardOverview
+				.getLayoutInflater()
+				.inflate(R.layout.leaderboard_section, null);
 
 		// noinspection ConstantConditions
-		LinearLayout linearLayout = (LinearLayout) mView
-				.findViewById(R.id.leaderboard_section_layout);
-		ImageView mLeaderboardIcon = (ImageView) mView
+		ImageView mLeaderboardIcon = (ImageView) mLeaderboardListItemView
 				.findViewById(R.id.leaderboard_icon);
-		TextView mLeaderboardScoreLabel = (TextView) mView
-				.findViewById(R.id.leaderboard_score_label);
-		TextView mLeaderboardScoreDisplay = (TextView) mView
-				.findViewById(R.id.leaderboard_score_display);
-		TextView mLeaderboardNotPlayed = (TextView) mView
-				.findViewById(R.id.leaderboard_not_played);
-		TextView mLeaderboardRankDisplay = (TextView) mView
-				.findViewById(R.id.leaderboard_rank_display);
 
 		// Get the leaderboard index value
 		int index = LeaderboardType.getIndex(mGridSize, mHideOperators,
@@ -78,38 +73,77 @@ public class LeaderboardOverviewListItem {
 				LeaderboardType.getResId(index));
 		mLeaderboardIcon.setImageResource(LeaderboardType.getIconResId(index));
 
+		leaderboardOverviewListItemType = setupView();
+		setupOnClickListener(leaderboardOverview);
+		setupOnLongClickListener(leaderboardOverview);
+	}
+
+	private LeaderboardOverviewListItemType setupView() {
 		// Retrieve leaderboard score from local database
 		LeaderboardRankRow leaderboardRankRow = new LeaderboardRankDatabaseAdapter()
 				.get(mLeaderboardId);
 		if (leaderboardRankRow == null
 				|| leaderboardRankRow.getScoreOrigin() == LeaderboardRankDatabaseAdapter.ScoreOrigin.NONE) {
-			mHasScore = false;
-			mLeaderboardScoreLabel.setVisibility(View.GONE);
-			mLeaderboardScoreDisplay.setVisibility(View.GONE);
-			mLeaderboardNotPlayed.setVisibility(View.VISIBLE);
-			mLeaderboardRankDisplay.setVisibility(View.GONE);
+			setupViewWithoutRankingInformation();
+			return LeaderboardOverviewListItemType.WITH_SCORE;
 		} else {
-			mHasScore = true;
-			mLeaderboardScoreLabel.setVisibility(View.VISIBLE);
-			mLeaderboardScoreDisplay.setText(Util
-					.durationTimeToString(leaderboardRankRow.getRawScore()));
-			mLeaderboardScoreDisplay.setVisibility(View.VISIBLE);
-			mLeaderboardNotPlayed.setVisibility(View.GONE);
-			mLeaderboardRankDisplay
-					.setText(leaderboardRankRow.getRankDisplay());
-			mLeaderboardRankDisplay.setVisibility(View.VISIBLE);
+			setupViewWithRankingInformation(leaderboardRankRow);
+			return LeaderboardOverviewListItemType.WITHOUT_SCORE;
 		}
+	}
 
-		// Set a listener on the leaderboard linear layout to navigate to
-		// the detail page of the leaderboard on Google Play Services.
+	private void setupViewWithoutRankingInformation() {
+		((TextView) mLeaderboardListItemView
+				.findViewById(R.id.leaderboard_score_label))
+				.setVisibility(View.GONE);
+		((TextView) mLeaderboardListItemView
+				.findViewById(R.id.leaderboard_score_display))
+				.setVisibility(View.GONE);
+		((TextView) mLeaderboardListItemView
+				.findViewById(R.id.leaderboard_not_played))
+				.setVisibility(View.VISIBLE);
+		((TextView) mLeaderboardListItemView
+				.findViewById(R.id.leaderboard_rank_display))
+				.setVisibility(View.GONE);
+	}
+
+	private void setupViewWithRankingInformation(
+			LeaderboardRankRow leaderboardRankRow) {
+		((TextView) mLeaderboardListItemView
+				.findViewById(R.id.leaderboard_score_label))
+				.setVisibility(View.VISIBLE);
+		((TextView) mLeaderboardListItemView
+				.findViewById(R.id.leaderboard_score_display)).setText(Util
+				.durationTimeToString(leaderboardRankRow.getRawScore()));
+		((TextView) mLeaderboardListItemView
+				.findViewById(R.id.leaderboard_score_display))
+				.setVisibility(View.VISIBLE);
+		((TextView) mLeaderboardListItemView
+				.findViewById(R.id.leaderboard_not_played))
+				.setVisibility(View.GONE);
+		((TextView) mLeaderboardListItemView
+				.findViewById(R.id.leaderboard_rank_display))
+				.setText(leaderboardRankRow.getRankDisplay());
+		((TextView) mLeaderboardListItemView
+				.findViewById(R.id.leaderboard_rank_display))
+				.setVisibility(View.VISIBLE);
+	}
+
+	private void setupOnClickListener(LeaderboardOverview leaderboardOverview) {
+		LinearLayout linearLayout = (LinearLayout) mLeaderboardListItemView
+				.findViewById(R.id.leaderboard_section_layout);
 		linearLayout.setClickable(true);
 		linearLayout.setOnClickListener(new OnClickLeaderboardListener(
 				leaderboardOverview));
+	}
 
+	private void setupOnLongClickListener(
+			LeaderboardOverview leaderboardOverview) {
 		// Attach a long click listener to start a new game for the selected
 		// leaderboard
-		mView.setOnLongClickListener(new OnLongClickLeaderboardListener(
-				leaderboardOverview));
+		mLeaderboardListItemView
+				.setOnLongClickListener(new OnLongClickLeaderboardListener(
+						leaderboardOverview));
 	}
 
 	/**
@@ -118,70 +152,58 @@ public class LeaderboardOverviewListItem {
 	 * leaderboard for this grid size and has enabled filter
 	 * "My leaderboards only".
 	 */
-	public LeaderboardOverviewListItem(LeaderboardOverview leaderboardOverview, int gridSize) {
-		mGridSize = gridSize;
+	public LeaderboardOverviewListItem(LeaderboardOverview leaderboardOverview,
+			int gridSize) {
+		mLeaderboardId = null;
+		mGridSize = 0;
+		mHideOperators = false;
+		mPuzzleComplexity = null;
+		leaderboardOverviewListItemType = LeaderboardOverviewListItemType.PLACEHOLDER_EMPTY_OVERVIEW;
 
-		TextView textView = new TextView(leaderboardOverview.getActivity());
-		textView.setLayoutParams(new ViewGroup.LayoutParams(
+		mLeaderboardListItemView = new TextView(
+				leaderboardOverview.getActivity());
+		mLeaderboardListItemView.setLayoutParams(new ViewGroup.LayoutParams(
 				ViewGroup.LayoutParams.WRAP_CONTENT,
 				ViewGroup.LayoutParams.WRAP_CONTENT));
-		textView.setText(leaderboardOverview.getResources().getString(
-				R.string.leaderboard_none_played, mGridSize));
-		textView.setTextSize(
+		((TextView) mLeaderboardListItemView).setText(leaderboardOverview
+				.getResources()
+				.getString(R.string.leaderboard_none_played, gridSize));
+		((TextView) mLeaderboardListItemView).setTextSize(
 				TypedValue.COMPLEX_UNIT_DIP,
 				(int) (leaderboardOverview.getResources().getDimension(
 						R.dimen.text_size_default) / leaderboardOverview
 						.getResources()
 						.getDisplayMetrics().density));
-
-		mView = textView;
 	}
 
 	public View getView() {
-		return mView;
+		return mLeaderboardListItemView;
 	}
 
 	public void setVisibility(int visibility) {
-		mView.setVisibility(visibility);
+		mLeaderboardListItemView.setVisibility(visibility);
 	}
 
 	public boolean hasNoScore() {
-		return !mHasScore;
+		throwExceptionIfCalledForPlaceholder();
+		return leaderboardOverviewListItemType == LeaderboardOverviewListItemType.WITHOUT_SCORE;
+	}
+
+	private void throwExceptionIfCalledForPlaceholder() {
+		if (leaderboardOverviewListItemType == LeaderboardOverviewListItemType.PLACEHOLDER_EMPTY_OVERVIEW) {
+			throw new IllegalStateException(
+					"Should not be called for the placeholder item.");
+		}
 	}
 
 	public boolean hasHiddenOperators() {
+		throwExceptionIfCalledForPlaceholder();
 		return mHideOperators;
 	}
 
-	private class OnLongClickLeaderboardListener implements
-			View.OnLongClickListener {
-		private final LeaderboardOverview leaderboardOverview;
-
-		public OnLongClickLeaderboardListener(
-				LeaderboardOverview leaderboardOverview) {
-			this.leaderboardOverview = leaderboardOverview;
-		}
-
-		@Override
-		public boolean onLongClick(View v) {
-			Activity activity = leaderboardOverview.getActivity();
-
-			// Finish the leaderboard activity.
-			activity.finish();
-
-			// Restart the main activity of MathDoku
-			Intent intent = PuzzleFragmentActivity
-					.createIntentToStartNewPuzzleFromSelectedLeaderboardFragment(
-							activity, mGridSize, mHideOperators,
-							mPuzzleComplexity);
-			leaderboardOverview.startActivity(intent);
-
-			Preferences.getInstance().increaseLeaderboardsGamesCreated();
-
-			return true;
-		}
-	}
-
+	/**
+	 * Navigate to the detail page on Google Play Services for this leaderboard.
+	 */
 	private class OnClickLeaderboardListener implements View.OnClickListener {
 		private final LeaderboardOverview leaderboardOverview;
 
@@ -191,7 +213,7 @@ public class LeaderboardOverviewListItem {
 		}
 
 		@Override
-		public void onClick(View v) {
+		public void onClick(View view) {
 			// Connect to the games client of the activity to start
 			// the Google Play Services leaderboard intent.
 			Activity activity = leaderboardOverview.getActivity();
@@ -215,6 +237,41 @@ public class LeaderboardOverviewListItem {
 					}
 				}
 			}
+		}
+	}
+
+	/**
+	 * Finishes the current activity and starts a new puzzle with the same size,
+	 * complexity and visibility of the operators as the leaderboard which was
+	 * long clicked.
+	 */
+	private class OnLongClickLeaderboardListener implements
+			View.OnLongClickListener {
+		private final LeaderboardOverview leaderboardOverview;
+
+		public OnLongClickLeaderboardListener(
+				LeaderboardOverview leaderboardOverview) {
+			this.leaderboardOverview = leaderboardOverview;
+		}
+
+		@Override
+		public boolean onLongClick(View view) {
+			Activity activity = leaderboardOverview.getActivity();
+
+			// Finish the leaderboard overview activity.
+			activity.finish();
+
+			// Start a new puzzle with the same size, complexity and visibility
+			// of the operators as the leaderboard which was long clicked.
+			Intent intent = PuzzleFragmentActivity
+					.createIntentToStartNewPuzzleFromSelectedLeaderboardFragment(
+							activity, mGridSize, mHideOperators,
+							mPuzzleComplexity);
+			leaderboardOverview.startActivity(intent);
+
+			Preferences.getInstance().increaseLeaderboardsGamesCreated();
+
+			return true;
 		}
 	}
 }
---
+++ app/src/test/java/net/mathdoku/plus/leaderboard/ui/LeaderboardOverviewListItemTest.java
@@ -0,0 +1,29 @@
+package net.mathdoku.plus.leaderboard.ui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import robolectric.RobolectricGradleTestRunner;
+
+@RunWith(RobolectricGradleTestRunner.class)
+public class LeaderboardOverviewListItemTest {
+	@Test
+	public void getView() throws Exception {
+
+	}
+
+	@Test
+	public void setVisibility() throws Exception {
+
+	}
+
+	@Test
+	public void hasNoScore() throws Exception {
+
+	}
+
+	@Test
+	public void hasHiddenOperators() throws Exception {
+
+	}
+}
