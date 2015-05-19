2e5cce57fc5e6252a2eeb81fcf90b277efbcf0e4
Paul Dingemans <paul@mathdoku.net>
2015-05-19 20:37:16 +0200

    Remove duplicate code by pulling methods up to CageComboGenerator

--- app/src/main/java/net/mathdoku/plus/gridsolving/combogenerator/CageComboGenerator.java
+++ app/src/main/java/net/mathdoku/plus/gridsolving/combogenerator/CageComboGenerator.java
@@ -6,9 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class CageComboGenerator {
-    protected final ComboGenerator comboGenerator;
-    protected final int gridSize;
+    private final ComboGenerator comboGenerator;
+    private final int gridSize;
     private List<CageCombo> allCageCombos;
+    private CageCombo cageCombo = new CageCombo();
 
 
     public CageComboGenerator(ComboGenerator comboGenerator) {
@@ -27,9 +28,43 @@ public abstract class CageComboGenerator {
         return allCageCombos;
     }
 
-    protected void add(CageCombo cageCombo) {
-        allCageCombos.add(cageCombo);
+    private void getCombosRecursively(int targetValue, int numberOfCellsRemaining) {
+        if (targetValue <= 0 || numberOfCellsRemaining <= 0) {
+            // Invalid combo
+            return;
+        }
+        if (numberOfCellsRemaining == 1 && targetValue > gridSize) {
+            // Unreachable targetValue
+            return;
+        }
+        if (numberOfCellsRemaining == 1) {
+            tryToAddTargetValueToCageCombo(targetValue);
+        } else {
+            expandCageComboWithNextValue(targetValue, numberOfCellsRemaining);
+        }
     }
 
-    abstract protected void getCombosRecursively(int targetValue, int numberOfCellsRemaining);
+    private void tryToAddTargetValueToCageCombo(int targetValue) {
+        cageCombo.append(targetValue);
+        if (comboGenerator.satisfiesConstraints(cageCombo)) {
+            // Clone the current cage combination as this instance will be manipulated in order to find other
+            // combo's.
+            allCageCombos.add(cageCombo.clone());
+        }
+        cageCombo.removeLastValue();
+    }
+
+    private void expandCageComboWithNextValue(int targetValue, int numberOfCellsRemaining) {
+        for (int cellValue = 1; cellValue <= gridSize; cellValue++) {
+            if (canExpandWithValue(targetValue, cellValue)) {
+                cageCombo.append(cellValue);
+                getCombosRecursively(getNextTargetValue(targetValue, cellValue), numberOfCellsRemaining - 1);
+                cageCombo.removeLastValue();
+            }
+        }
+    }
+
+    protected abstract boolean canExpandWithValue(int targetValue, int cellValue);
+
+    protected abstract int getNextTargetValue(int targetValue, int cellValue);
 }
--- app/src/main/java/net/mathdoku/plus/gridsolving/combogenerator/MultiplyCageComboGenerator.java
+++ app/src/main/java/net/mathdoku/plus/gridsolving/combogenerator/MultiplyCageComboGenerator.java
@@ -1,38 +1,18 @@
 package net.mathdoku.plus.gridsolving.combogenerator;
 
 /* package private */ class MultiplyCageComboGenerator extends CageComboGenerator {
-    private CageCombo cageCombo = new CageCombo();
 
     public MultiplyCageComboGenerator(ComboGenerator comboGenerator) {
         super(comboGenerator);
     }
 
     @Override
-    protected void getCombosRecursively(int targetValue, int numberOfCellsRemaining) {
-        if (targetValue <= 0 || numberOfCellsRemaining <= 0) {
-            // Invalid combo
-            return;
-        }
-        if (numberOfCellsRemaining == 1 && targetValue > gridSize) {
-            // Unreachable targetValue
-            return;
-        }
-        if (numberOfCellsRemaining == 1) {
-            cageCombo.append(targetValue);
-            if (comboGenerator.satisfiesConstraints(cageCombo)) {
-                // Clone the current cage combination as this instance will be manipulated in order to find other
-                // combo's.
-                add(cageCombo.clone());
-            }
-            cageCombo.removeLastValue();
-        } else {
-            for (int cellValue = 1; cellValue <= gridSize; cellValue++) {
-                if (targetValue % cellValue == 0) {
-                    cageCombo.append(cellValue);
-                    getCombosRecursively(targetValue / cellValue, numberOfCellsRemaining - 1);
-                    cageCombo.removeLastValue();
-                }
-            }
-        }
+    protected boolean canExpandWithValue(int targetValue, int cellValue) {
+        return targetValue % cellValue == 0;
+    }
+
+    @Override
+    protected int getNextTargetValue(int targetValue, int cellValue) {
+        return targetValue / cellValue;
     }
 }
